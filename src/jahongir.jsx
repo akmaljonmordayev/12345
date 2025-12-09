b638a559daa2b917ca8916c1ffe1432a0e55a0ed
abrorkulov <randacc2025@gmail.com>
2025-12-09 16:07:08 +0500

    commentariya

---
+++ src/jahongir.jsx
@@ -0,0 +1,15 @@
+let a = `12`
+let b = 12
+console.log(a + b);
+
+import React from 'react'
+
+function jahongir() {
+  return (
+    <div>
+      jahongir
+    </div>
+  )
+}
+
+export default jahongir
